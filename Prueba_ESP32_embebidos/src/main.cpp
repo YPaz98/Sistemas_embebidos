d454f824750efd03db54eeb0eb6fd103e2293f95
YPaz98 <yeisson.paz01@uceva.edu.co>
2024-08-20 21:04:57 -0500

    Punteros

--- Prueba_ESP32_embebidos/src/main.cpp
+++ Prueba_ESP32_embebidos/src/main.cpp
@@ -1,19 +1,26 @@
+// Prueba, practicando punteros.
+
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int numero = 10; 
+void duplicar(int *puntero);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+    Serial.begin(115200);
 }
 
 void loop() {
-  // Esto es una prueba sencilla para subir cambios a GitHub
- //  Proyecto con ESP32
+
+    Serial.print("Numero = " + string(numero));
+    duplicar(&numero); // & numero, significa que le pasa la dirección de memoria de la variable número.  
+                      //  Operador, dirección.
+
+    delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void duplicar(int *puntero){
+
+    *puntero = *puntero * 2;
+    Serial.print("Numero duplicado: "+string(*puntero));
 }
